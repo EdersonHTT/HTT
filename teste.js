bade614482e4c290802b38853a9a6c78c80c3e37
EdersonHTT <edersondasilvaschmidt@gmail.com>
2024-04-18 21:47:12 -0300

    Testando

---
+++ teste.js
@@ -0,0 +1,7 @@
+const nomeEu = Ederson
+const sobrenomeEu = Silva.Schmidt
+const idadeEu = 16
+const estudanteEu = true
+alert("Proprietario: "+ nomeEu + "da "+ sobrenomeEu+ "\nIdade: "+ idadeEu+ "\nEstudando: "+ estudanteEu)
+console.log("Proprietario: "+ nomeEu + "da "+ sobrenomeEu+ "\nIdade: "+ idadeEu+ "\nEstudando: "+ estudanteEu)
+
